191b570cf979891328c735a9849af222c0021c53
Nathan <nathan.w.pulver@gmail.com>
2022-11-23 15:46:15 -0800

    added different blog category pages

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -10,9 +10,6 @@ export default function Greeting({messages}) {
   return (
     <div> 
       <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>
-        New Greeting
-      </button>
     </div>
   );
 }
