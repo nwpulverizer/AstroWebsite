cca90f04cf318ab51028b16af4d715a8a8ad0b0c
nwpulverizer <nathan.w.pulver@gmail.com>
2025-02-10 22:29:24 -0800

    changed some formatting of the site.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,9 +1,10 @@
-import rss from '@astrojs/rss';
+import rss from "@astrojs/rss";
 
-export const get = () => rss({
+export const get = () =>
+  rss({
     title: "Nathan's Hobbies | Blog",
-    description: 'My journey leasrning Astro',
-    site: 'https://nathanshobbies.com',
-    items: import.meta.glob('./**/*.md'),
+    description: "A blog from a dude bein",
+    site: "https://nathanshobbies.com",
+    items: import.meta.glob("./**/*.md"),
     customData: `<language>en-us</language>`,
-})
+  });
